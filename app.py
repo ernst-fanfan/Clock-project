d9b1b4d85cf01ca2cc503bd1e16de98877472b39
ernst-fanfan <rfanfan@gmail.com>
2025-02-08 12:17:46 -0500

    faster updates

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import requests
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
+import json
+import time
 
 app = Flask(__name__)
 app.secret_key = 'BAD_SECRET_KEY'
@@ -12,14 +14,9 @@ def get_time_for_timezone(timezone):
 
 @app.route('/', methods=['GET'])
 def index():
-    timezones = ["America/New_York"] #["America/New_York", "Europe/London", "Asia/Tokyo"]
+    timezones = ["America/New_York", "Europe/London", "Asia/Tokyo"]
     times = {tz: get_time_for_timezone(tz) for tz in timezones}
-
-    return render_template('index.html', **{
-        "title": "Time",
-        "times": times,
-        "footer": "Created by Ernst Fanfan",
-    })
+    return render_template('index.html', title="Time", times=times, footer="Created by Ernst Fanfan")
 
 if __name__ == '__main__':
     app.run(debug=True)
