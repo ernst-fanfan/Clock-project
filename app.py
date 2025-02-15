dc3e4b9ef43b5aa51732cbde73b3cf75bee4c1b6
ernst-fanfan <rfanfan@gmail.com>
2025-02-15 12:59:54 -0500

    add TZ selection

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import requests
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, request, jsonify
 import json
 import time
 
@@ -12,11 +12,22 @@ def get_time_for_timezone(timezone):
     response.raise_for_status()
     return response.json()
 
-@app.route('/', methods=['GET'])
+def get_all_timezones():
+    response = requests.get("https://timeapi.io/api/TimeZone/AvailableTimeZones")
+    response.raise_for_status()
+    return response.json()
+
+@app.route('/timezones', methods=['GET'])
+def timezones():
+    timezones = get_all_timezones()
+    return jsonify(timezones)
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    timezones = ["America/New_York", "Europe/London", "Asia/Tokyo"]
-    times = {tz: get_time_for_timezone(tz) for tz in timezones}
-    return render_template('index.html', title="Time", times=times, footer="Created by Ernst Fanfan")
+    all_timezones = get_all_timezones()
+    selected_timezones = request.form.getlist('timezones') if request.method == 'POST' else ["America/New_York", "Europe/London", "Asia/Tokyo"]
+    times = {tz: get_time_for_timezone(tz) for tz in selected_timezones}
+    return render_template('index.html', title="Time", times=times, all_timezones=all_timezones, selected_timezones=selected_timezones, footer="Created by Ernst Fanfan")
 
 if __name__ == '__main__':
     app.run(debug=True)
